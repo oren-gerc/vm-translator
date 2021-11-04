331980e18eae0e41483755795ef72857e58f54aa
oren_gerc <orengerc@gmail.com>
2021-11-04 14:59:16 +0200

    added all .py files

---
+++ Parser.py
@@ -0,0 +1,89 @@
+"""This file is part of nand2tetris, as taught in The Hebrew University,
+and was written by Aviv Yaish according to the specifications given in  
+https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
+and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
+Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+import utils
+
+
+# CHANGE PARSING! NEEDS TO SUPPORT WHITESPACES INSIDE THE LINE ITSELF
+def clean(commands):
+    # strip white spaces from commands
+    for i, command in enumerate(commands):
+        commands[i] = command.replace(" ", "")
+        if "//" in command:
+            del_ind = command.find("/")
+            commands[i] = command[:del_ind]
+            commands[i] = commands[i].replace(" ", "")
+
+    value_to_remove = ""
+    commands = [command for command in commands if command != value_to_remove]
+    return commands
+
+
+class Parser:
+    """
+    Handles the parsing of a single .vm file, and encapsulates access to the
+    input code. It reads VM commands, parses them, and provides convenient 
+    access to their components. 
+    In addition, it removes all white space and comments.
+    """
+
+    def __init__(self, input_file: typing.TextIO) -> None:
+        """Gets ready to parse the input file.
+
+        Args:
+            input_file (typing.TextIO): input file.
+        """
+        self.commands = clean(input_file.read().splitlines())
+        self.command_index = 0
+        self.command = self.commands[self.command_index]
+
+    def has_more_commands(self) -> bool:
+        """Are there more commands in the input?
+
+        Returns:
+            bool: True if there are more commands, False otherwise.
+        """
+        return (self.command_index + 1) != len(self.commands)
+
+    def advance(self) -> None:
+        """Reads the next command from the input and makes it the current 
+        command. Should be called only if has_more_commands() is true. Initially
+        there is no current command.
+        """
+        self.command_index += 1
+        self.command = self.commands[self.command_index]
+
+    def command_type(self) -> str:
+        """
+        Returns:
+            str: the type of the current VM command.
+            "C_ARITHMETIC" is returned for all arithmetic commands.
+            For other commands, can return:
+            "C_PUSH", "C_POP", "C_LABEL", "C_GOTO", "C_IF", "C_FUNCTION",
+            "C_RETURN", "C_CALL".
+        """
+        return utils.command_type_dict[self.command]
+
+    def arg1(self) -> str:
+        """
+        Returns:
+            str: the first argument of the current command. In case of 
+            "C_ARITHMETIC", the command itself (add, sub, etc.) is returned. 
+            Should not be called if the current command is "C_RETURN".
+        """
+        if self.command_type() == "C_ARITHMETIC":
+            return self.command
+        return self.command.split()[1]
+
+    def arg2(self) -> int:
+        """
+        Returns:
+            int: the second argument of the current command. Should be
+            called only if the current command is "C_PUSH", "C_POP", 
+            "C_FUNCTION" or "C_CALL".
+        """
+        return int(self.command.split()[2])
---
+++ utils.py
@@ -0,0 +1,4 @@
+command_type_dict = {"add": "C_ARITHMETIC", "sub": "C_ARITHMETIC", "neg": "C_ARITHMETIC",
+                     "eq": "C_ARITHMETIC", "gt": "C_ARITHMETIC", "lt": "C_ARITHMETIC",
+                     "and": "C_ARITHMETIC", "or": "C_ARITHMETIC", "not": "C_ARITHMETIC",
+                     "push": "C_PUSH", "pop": "C_POP"}
